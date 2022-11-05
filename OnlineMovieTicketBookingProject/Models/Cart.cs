e3b034be8ce1f29b26751f3317a0911dc4af7795
Darren De Doncker <s213402432@Mandela.ac.za>
2022-11-05 14:53:25 +0200

    Add project files.

---
+++ OnlineMovieTicketBookingProject/Models/Cart.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineMovieTicketBookingProject.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string seatNo { get; set; }
+        public string UserId { get; set; }
+        public DateTime date { get; set; }
+        public int Amount { get; set; }
+        public int MovieId { get; set; }
+    }
+}
---
+++ OnlineMovieTicketBookingProject/Models/MovieDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineMovieTicketBookingProject.Models
+{
+    public class MovieDetails
+    {
+        public int Id { get; set; }
+        public string Movie_Name { get; set; }
+        public string Movie_Description { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public string MoviePicture { get; set; }
+    }
+}
